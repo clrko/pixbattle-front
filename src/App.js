dd73907e049fadcd0820f88610eb5d987fbbf0c3
Aristide-O <heavenly_7@hotmail.fr>
2020-06-10 16:16:22 +0200

    created basic route architecture + creation of the landing page component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import LandingPage from './components/LandingPage.js'
 
 function App () {
   return (
-    <div>Hello</div>
+    <div>
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+      </Switch>
+    </div>
   )
 }
 
