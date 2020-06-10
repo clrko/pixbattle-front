3599e9c71f37a9b754229d58d791e3e5ba851a75
Aristide-O <heavenly_7@hotmail.fr>
2020-06-10 18:25:32 +0200

    created community box with text inside

---
+++ src/components/LP_CommunityBox.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './LP_CommunityBox.css'
+
+function LpCommunityBox () {
+  return (
+    <div className='communityBoxLP'>
+      <div className='communityBoxLP-text'>
+        <h1 className='community-h1'>
+            Community
+        </h1>
+        <p className='community-p'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default LpCommunityBox
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import logoLP from '../Logo/logo-web-transparent.png'
 import './LandingPage.css'
+import LpCommunityBox from './Lp_CommunityBox'
 
 function LandingPage () {
   return (
     <div className='landingPage'>
       <div className='logo-div'>
         <img src={logoLP} className='logoLP' />
+      </div>
+      <div>
+        <LpCommunityBox />
       </div>
     </div>
   )
