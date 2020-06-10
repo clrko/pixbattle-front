db812b6b57d3e3784cdfb47a0ac917c1710a046b
Aristide-O <heavenly_7@hotmail.fr>
2020-06-10 17:08:00 +0200

    added logo to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import logoLP from '../Logo/logo-web-transparent.png'
 import './LandingPage.css'
 
 function LandingPage () {
   return (
     <div className='landingPage'>
         LandingPage
+      <img src={logoLP} className='logo-landingPage' />
     </div>
   )
 }
