371340083cb0f3ef047ce7c3fdc77a9b4106c284
Claire Kodia <claire.kodia@gmail.com>
2020-06-15 17:23:23 +0200

    Added comments todo

--- src/components/pages/BattleCreationTheme.js
+++ src/components/pages/BattleCreationTheme.js
@@ -64,15 +64,15 @@ class BattleCreationTheme extends Component {
         <div className='createBattleBanner'>Créer une battle</div>
         <div className='cardBattle'>
           <h1>1. Choisis un thème</h1>
-          <p>Prédéfini :</p>
+          <p>Prédéfini :</p> {/* Ajouter bouton pour sauvgarder dans le state du store le theme_id */}
           <div> {refreshed ? themes.map(theme => <button className='themeButton' key={theme.theme_id}>{theme.theme_name}</button>) : themes.slice(0, 5).map(theme => <button className='themeButton' key={theme.theme_id}>{theme.theme_name}</button>)}</div>
           <button className={refreshed ? 'refreshed' : 'refreshButton'} onClick={this.handleRefresh} type='button'>Plus de thèmes</button>
           <div>
-            <button className='cancelButton' type='button'>Annuler</button>
-            <button className='validateButton' type='button'>Suivant</button>
+            <button className='cancelButton' type='button'>Annuler</button> {/* Ajouter lien vers userpage */}
+            <button className='validateButton' type='button'>Suivant</button> {/* Ajotuer lien vers next page choix des règles */}
           </div>
         </div>
-      </div>
+      </div> /* bloquer le bouton */
     )
   }
 }
