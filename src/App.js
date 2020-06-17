b0578fa3576fef78a5605e7babf5ba0d46c6e5b3
Claire Kodia <claire.kodia@gmail.com>
2020-06-17 18:08:26 +0200

    Created the battle rule component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import BattleCreationRules from './components/pages/BattleCreationRules'
+import BattleCreationTheme from './components/pages/BattleCreationTheme'
 import LandingPage from './components/pages/LandingPage.js'
 import LoginRegistrationFormPage from './components/pages/LoginRegistrationFormPage.js'
-import BattleCreationTheme from './components/pages/BattleCreationTheme'
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={LandingPage} />
-        <Route path='/LoginRegistration' component={LoginRegistrationFormPage} />
+        <Route path='/battlecreationrules' component={BattleCreationRules} />
         <Route path='/battlecreationtheme' component={BattleCreationTheme} />
+        <Route path='/LoginRegistration' component={LoginRegistrationFormPage} />
       </Switch>
     </div>
   )
---
+++ src/components/pages/BattleCreationRules.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import './BattleCreationPage.css'
+
+const themes = [
+  {
+    theme_id: 26,
+    theme_name: 'Petit bonheur'
+  },
+  {
+    theme_id: 33,
+    theme_name: 'Lecture'
+  },
+  {
+    theme_id: 147,
+    theme_name: 'Tricolore'
+  },
+  {
+    theme_id: 133,
+    theme_name: 'Chiffres et nombres'
+  },
+  {
+    theme_id: 97,
+    theme_name: 'De l’autre côté'
+  },
+  {
+    theme_id: 110,
+    theme_name: 'Froid'
+  },
+  {
+    theme_id: 83,
+    theme_name: 'Petit trésor'
+  },
+  {
+    theme_id: 56,
+    theme_name: 'Balle & ballon'
+  },
+  {
+    theme_id: 20,
+    theme_name: 'Miroir'
+  },
+  {
+    theme_id: 143,
+    theme_name: 'Arbre'
+  }
+]
+
+class BattleCreationTheme extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      refreshed: false,
+      selectedTheme: 0
+    }
+    this.handleRefresh = this.handleRefresh.bind(this)
+    this.handleThemeChange = this.handleThemeChange.bind(this)
+  }
+
+  handleRefresh () {
+    this.setState({ refreshed: !this.state.refreshed })
+  }
+
+  handleThemeChange (event) {
+    this.setState({
+      selectedTheme: event.target.id
+    })
+  }
+
+  render () {
+    const { refreshed, selectedTheme } = this.state
+    return (
+      <div className='battleCreation-page'>
+        <div className='battleCreation-banner'>Créer une battle</div>
+        <div className='cardBattle'>
+          <h1>1. Choisis un thème</h1>
+          <div className='battleCreation-themeContainer'> {refreshed ? themes.map((theme, i) => <><input type='radio' name='themeButton' value={theme.theme_name} checked={selectedTheme === theme.theme_id} onChange={this.handleThemeChange} id={theme.theme_id} key={theme.theme_id} /><label htmlFor={theme.theme_id} className='themeButton battle-btn' key={i}>{theme.theme_name}</label></>) : themes.slice(0, 5).map((theme, i) => <><input type='radio' name='themeButton' value={theme.theme_name} checked={selectedTheme === theme.theme_id} onChange={this.handleThemeChange} id={theme.theme_id} key={theme.theme_id} /><label htmlFor={theme.theme_id} className='themeButton battle-btn' key={i}>{theme.theme_name}</label></>)}</div>
+          <button className={refreshed ? 'refreshed' : 'refreshButton'} onClick={this.handleRefresh} type='button'>Plus de thèmes</button>
+          <div className='battleCreation-btnContainer'>
+            <button className='battleCreation-cancelButton battle-btn' type='button'>Annuler</button> {/* Ajouter lien vers userpage */}
+            <button className='battleCreation-validateButton battle-btn' type='button'>Suivant</button> {/* Ajotuer lien vers next page choix des règles */}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BattleCreationTheme
