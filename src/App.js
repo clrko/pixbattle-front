7a5a77414e6469e8410a6aa0476b3f5e358b82aa
Claire Kodia <claire.kodia@gmail.com>
2020-06-17 21:38:57 +0200

    Managed to save the rule_id of the selected items into an array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import BattleCreationRules from './components/pages/BattleCreationRules'
+import BattleCreationRule from './components/pages/BattleCreationRule'
 import BattleCreationTheme from './components/pages/BattleCreationTheme'
 import LandingPage from './components/pages/LandingPage.js'
 import LoginRegistrationFormPage from './components/pages/LoginRegistrationFormPage.js'
@@ -10,7 +10,7 @@ const App = () => {
     <div>
       <Switch>
         <Route exact path='/' component={LandingPage} />
-        <Route path='/battlecreationrules' component={BattleCreationRules} />
+        <Route path='/battlecreationrule' component={BattleCreationRule} />
         <Route path='/battlecreationtheme' component={BattleCreationTheme} />
         <Route path='/LoginRegistration' component={LoginRegistrationFormPage} />
       </Switch>
