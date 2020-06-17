b234fb69e523608edcb4f007ac86de6038be7ae3
Claire Kodia <claire.kodia@gmail.com>
2020-06-17 15:02:47 +0200

    Added actions LOGIN and LOGOUT and destructred the action to get the userID, avatar and username

--- src/store/userReducer.js
+++ src/store/userReducer.js
@@ -1,7 +1,15 @@
+import { LOGIN, LOGOUT } from './action-types'
+
+// logout: { type: 'LOGOUT' }
+// login:  { type: 'LOGIN', userId, avatar, username }
 const userReducer = (state = null, action) => {
   switch (action.type) {
-    case '':
-      break
+    case LOGIN: {
+      const { userId, avatar, username } = action
+      return { userId, avatar, username }
+    }
+    case LOGOUT:
+      return null
     default:
       return state
   }
