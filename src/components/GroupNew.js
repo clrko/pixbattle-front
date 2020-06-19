35d754eab16ee7e006706b176feb0b1611fe0256
Lola <lola.donval@gmail.com>
2020-06-19 21:15:04 +0200

    add button to remove emails

--- src/components/GroupNew.js
+++ src/components/GroupNew.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
+// import store from '../store/userReducer'
 
 import './GroupNew.css'
 
@@ -17,17 +18,19 @@ class GroupNew extends Component {
 
   handleAddEmail = (e) => {
     e.preventDefault()
-    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(this.state.email)) {
-      if (this.state.allEmails.includes(this.state.email) === false) {
-        const allEmailsTemp = this.state.allEmails
-        const newEmail = this.state.email
+    const { allEmails, email, count } = this.state
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      if (allEmails.includes(email) === false) {
+        const allEmailsTemp = allEmails
+        const newEmail = email
         allEmailsTemp.push(newEmail)
         this.setState({
           allEmails: allEmailsTemp,
-          count: this.state.count + 1,
+          count: count + 1,
           email: ''
         })
-      } else if (this.state.allEmails.includes(this.state.email)) {
+        console.log(allEmails)
+      } else if (allEmails.includes(email)) {
         alert('Tu as déjà invité cette personne')
       } else {
         alert('Quelque chose s\'est mal passé')
@@ -37,18 +40,27 @@ class GroupNew extends Component {
     }
   }
 
+  handleRemoveEmail = email => {
+    this.setState(({ allEmails }) => ({
+      allEmails: allEmails.filter(e => e !== email)
+    }))
+  }
+
   handleCreateGroup = (e) => {
     e.preventDefault()
-    console.log(this.state.groupName)
+
     const { allEmails, groupName } = this.state
-    axios
-      .post(`${process.env.REACT_APP_SERVER_URL}group-creation`, allEmails.join(), groupName)
-      .then(res => {
-        console.log(res)
-      })
+    // let currentValue = store.getState()
+    console.log(allEmails, groupName)
+    // axios
+    //   .post(`${process.env.REACT_APP_SERVER_URL}group-creation`, allEmails.join(), groupName, currentValue)
+    //   .then(res => {
+    //     console.log(res, allEmails.join(), groupName, currentValue)
+    //   })
   }
 
   render () {
+    const { groupName, email, count, allEmails } = this.state
     return (
       <div className='NewGroup'>
         <form className='form-container'>
@@ -60,13 +72,13 @@ class GroupNew extends Component {
               name='groupName'
               onChange={this.handleChange}
               placeholder='Nom du groupe'
-              value={this.state.groupName}
+              value={groupName}
               required
               minLength='5'
               maxLength='25'
             />
             <p className={
-              this.state.groupName.length < 5
+              groupName.length < 5
                 ? 'info'
                 : 'info green'
             }
@@ -82,7 +94,7 @@ class GroupNew extends Component {
                   type='text'
                   className='new-group-input'
                   onChange={this.handleChange}
-                  value={this.state.email}
+                  value={email}
                   name='email'
                   placeholder='Email'
                 />
@@ -90,10 +102,10 @@ class GroupNew extends Component {
                   type='submit'
                   className='add-user'
                   onClick={this.handleAddEmail}
-                  disabled={this.state.count >= 12}
+                  disabled={count >= 12}
                 >
                   <i className={
-                    this.state.count < 12
+                    count < 12
                       ? 'fas fa-plus-circle email-abled'
                       : 'fas fa-plus-circle email-disabled'
                   }
@@ -101,7 +113,7 @@ class GroupNew extends Component {
                 </button>
               </div>
               <p className={
-                this.state.allEmails.length < 3 ? 'info' : 'info green'
+                allEmails.length < 3 ? 'info' : 'info green'
               }
               >
                 Entre 4 à 12 personnes
@@ -110,29 +122,32 @@ class GroupNew extends Component {
             <ul className='emails-container'>
               <li className='emails-list'>current_username</li>
               {
-                this.state.allEmails.map((e, i) => (
+                allEmails.map((email, i) => (
                   <li
                     className='emails-list'
                     key={i}
                   >
-                    {e}
+                    {email}
+                    <button className='remove-email' onClick={() => this.handleRemoveEmail(email)}>
+                      <i className='fas fa-times-circle' />
+                    </button>
                   </li>
                 ))
               }
             </ul>
             <div className='bottom-new-group'>
               <p className='members'>
-                Déjà {this.state.count} {this.state.count === 1 ? 'personne inscrite !' : 'personnes inscrites !'}
+                Déjà {count} {count === 1 ? 'personne inscrite !' : 'personnes inscrites !'}
               </p>
               <button
                 type='submit'
                 className={
-                  this.state.count > 3
+                  count > 3
                     ? 'create-group create-abled'
                     : 'create-group create-disabled'
                 }
                 onClick={this.handleCreateGroup}
-                disabled={this.state.count < 4}
+                disabled={count < 3}
               >
                 Créé ta battle !
               </button>
