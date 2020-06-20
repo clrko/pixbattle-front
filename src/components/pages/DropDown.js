371e18471693dea9a936bd317824600c7a68e012
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-06-20 11:31:09 +0200

    updates DropDown.js

--- src/components/pages/DropDown.js
+++ src/components/pages/DropDown.js
@@ -3,16 +3,18 @@ import './DropDown.css'
 
 class DropDown extends React.Component {
   handleChange = e => {
-    this.props.history.push(`/${e.target.value}`)
+    this.props.history.push(e.target.value)
   }
 
   render () {
+    const { match } = this.props
+    const selectedOption = match.path
     return (
       <div className='background-DropDown'>
-        <select className='DropDown' onChange={this.handleChange}>
-          <option value='MyProfile'>Mon Profil</option>
-          <option value='MyRanking'>Mon Classement</option>
-          <option value='MyPictures'>Mes Photos</option>
+        <select value={selectedOption} className='DropDown' onChange={this.handleChange}>
+          <option value='/MyProfile'>Mon Profil</option>
+          <option value='/MyRanking'>Mon Classement</option>
+          <option value='/MyPictures'>Mes Photos</option>
         </select>
       </div>
     )
