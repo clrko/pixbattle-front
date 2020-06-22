8a385da410fdefe8c1f7097b6f865fab84e0b2b1
Aristide-O <heavenly_7@hotmail.fr>
2020-06-22 20:24:04 +0200

    deleted registrationUserReducer

--- src/store/action-types.js
+++ src/store/action-types.js
@@ -1,4 +1,2 @@
 export const LOGIN = 'LOGIN'
 export const LOGOUT = 'LOGOUT'
-export const REGISTERED = 'REGISTERED'
-export const UNREGISTERED = 'UNREGISTERED'
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import userReducer from './userReducer'
-import registrationUserReducer from './registrationUserReducer'
 
 export default combineReducers({
-  userReducer,
-  registrationUserReducer
+  userReducer
 })
