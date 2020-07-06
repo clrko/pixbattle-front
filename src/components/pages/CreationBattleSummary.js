e20f6599761659f10e82472da06acadbaa1ea6da
Claire Kodia <claire.kodia@gmail.com>
2020-07-06 11:55:02 +0200

    managed to extract data from redux and send them to the back

--- src/components/pages/CreationBattleSummary.js
+++ src/components/pages/CreationBattleSummary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import axios from 'axios'
+import axios from 'axios'
+import { REMOVE_ALL } from '../../store/action-types'
 
 const mapStateToProps = state => {
   const { battleCreation } = state
@@ -8,42 +9,39 @@ const mapStateToProps = state => {
 }
 
 const CreationBattleSummary = ({ battleCreation }) => {
-  console.log('battle creation', battleCreation)
-  /* const handleClick = e => {
+  const handleClick = e => {
     axios.post(`${process.env.REACT_APP_SERVER_URL}/battle-creation`,
       {
         headers: {
           'x-access-token': localStorage.getItem('token')
         },
         body: {
-          'groupId': '',
-          'themeId': '',
-          'rulesId': '',
-          'deadline': ''
+          groupId: parseInt(battleCreation[0].groupId),
+          themeId: parseInt(battleCreation[1].themeId),
+          rulesId: battleCreation[2].map(rule => rule.rule_id),
+          deadline: battleCreation[3]
         }
       }
     ).then(res => {
-      if (res.status === 200) {
+      if (res.status === 201) {
         const { dispatch } = this.props
-        dispatch({ type: REMOVE_ALL, selectedTheme })
+        dispatch({ type: REMOVE_ALL })
       }
     })
-  } */
+  }
 
   return (
-    <div>
-      <h1>Vous êtes sur le point de créer la battle suivante</h1>
-      <h2>Thème de la battle</h2>
-      <p>{battleCreation[1].themeName}</p>
-      <h2>Personnalisation de la battle</h2>
-      {battleCreation[2].map(rule => <p key={rule.rule_id}>{rule.rule_name}</p>)}
-      <h2>Date limite choisie</h2>
+    <div className='cardBattle-summary'>
+      <h1 className='cardBattle-color'>Récapitulatif</h1>
+      <h2 className='cardBattle-color'>Thème choisi</h2>
+      <p className='battle-optionButton battle-btn'>{battleCreation[1].themeName}</p>
+      <h2 className='cardBattle-color'>Contraintes à respecter</h2>
+      {battleCreation[2].map(rule => <p key={rule.rule_id} className='battle-optionButton battle-btn'>{rule.rule_name}</p>)}
+      <h2 className='cardBattle-color'>Date limite choisie</h2>
       <p>{battleCreation[3]}</p>
-      <button>Confirmer</button>
+      <button className='battleCreation-validateButton battle-btn' onClick={handleClick}>Confirmer</button>
     </div>
   )
 }
-
-/* Il faut mettre dans des state / variable la deadline, ou dnas les paramètres de axios la deadline, le group_id, theme/id, tables appelé allRules */
 
 export default connect(mapStateToProps)(CreationBattleSummary)
