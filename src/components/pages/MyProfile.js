273199cae3d329179a6eedbc837f3495250945b4
Claire Kodia <claire.kodia@gmail.com>
2020-07-08 15:26:37 +0200

    Changed to class component to function stateless component

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -14,8 +14,8 @@ const mapStateToProps = state => {
   return { user }
 }
 
-class MyProfile extends React.Component {
-  handleCreateGroupe = e => {
+const MyProfile = ({ user }) => {
+  const handleCreateGroupe = e => {
     e.preventDefault()
     const { history } = this.props
     axios
@@ -33,31 +33,28 @@ class MyProfile extends React.Component {
         })
       })
   }
-
-  render () {
-    return (
-      <div className='background-MyProfile'>
-        <Navbar />
-        <Menu />
-        <div className='window-MyProfile'>
-          <div className='name-fa-star-MyProfile'>
-            <h1 className='name-MyProfile'>{this.props.user.username}<i className='fa fa-star fa-lg star-icon-MyProfile'><p className='p-victory-point-MyProfile'>20</p></i></h1>
-          </div>
-          <div className='div-avatar-Myprofile'>
-            <img className='avatar-MyProfile' src={this.props.user.avatar} alt='avatar' />
-          </div>
-          <div className='div-informations-MyProfile'>
-            <p className='p-victory-MyProfile'>4 victoires</p>
-            <p className='p-picture-MyProfile'>12 photos</p>
-            <p className='p-group-MyProfile'>2 groupes</p>
-            <p className='p-friend-MyProfile'>18 amis</p>
-            <button className='button-createdNewGroup-MyProfile' onClick={this.handleCreateGroupe}>Créer un nouveau groupe</button>
-          </div>
+  return (
+    <div className='background-MyProfile'>
+      <Navbar />
+      <Menu />
+      <div className='window-MyProfile'>
+        <div className='name-fa-star-MyProfile'>
+          <h1 className='name-MyProfile'>{user.username}<i className='fa fa-star fa-lg star-icon-MyProfile'><p className='p-victory-point-MyProfile'>20</p></i></h1>
         </div>
-        <StickyFooter />
+        <div className='div-avatar-Myprofile'>
+          <img className='avatar-MyProfile' src={user.avatar} alt='avatar' />
+        </div>
+        <div className='div-informations-MyProfile'>
+          <p className='p-victory-MyProfile'>4 victoires</p>
+          <p className='p-picture-MyProfile'>12 photos</p>
+          <p className='p-group-MyProfile'>2 groupes</p>
+          <p className='p-friend-MyProfile'>18 amis</p>
+          <button className='button-createdNewGroup-MyProfile' onClick={handleCreateGroupe}>Créer un nouveau groupe</button>
+        </div>
       </div>
-    )
-  }
+      <StickyFooter />
+    </div>
+  )
 }
 
 export default connect(mapStateToProps)(MyProfile)
