76b14e7450fa54d76a8164150e62c253f7a77fb6
Claire Kodia <claire.kodia@gmail.com>
2020-07-10 08:23:59 +0200

    Created the MyBattle window and card structure

--- src/components/pages/MyBattles.js
+++ src/components/pages/MyBattles.js
@@ -10,11 +10,17 @@ const Menu = withRouter(DropDown)
 class MyBattles extends React.Component {
   render () {
     return (
-      <div className='background-MyBattles'>
+      <div className='MyBattles-background'>
         <Navbar />
         <Menu />
-        <div className='window-MyBattles'>
-          <p>Mes Battles</p>
+        <div className='MyBattles-cardList'>
+          <div className='MyBattles-card'>
+            <h2 className='MyBattles-card-title'>Thèmes: Reflets</h2>
+            <p>Temps écoulé</p>
+            <p>Progress bar</p>
+            <button className='MyBattles-card-button'>Vote</button>
+            <p className='MyBattles-card-group'>Team Pix battle</p>
+          </div>
         </div>
         <StickyFooter />
       </div>
