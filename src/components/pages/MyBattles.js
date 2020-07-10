0a45ad0d20e5bb31dd21b6369afdffd3cab8b4d2
Claire Kodia <claire.kodia@gmail.com>
2020-07-10 08:29:39 +0200

    REplaced the div section by a component import of My BattlesCardList

--- src/components/pages/MyBattles.js
+++ src/components/pages/MyBattles.js
@@ -2,30 +2,21 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import Navbar from '../shared/Navbar'
 import DropDown from '../shared/DropDown'
+import MyBattlesCardList from './MyBattlesCardList'
 import StickyFooter from '../shared/StickyFooter'
 import './MyBattles.css'
 
 const Menu = withRouter(DropDown)
 
-class MyBattles extends React.Component {
-  render () {
-    return (
-      <div className='MyBattles-background'>
-        <Navbar />
-        <Menu />
-        <div className='MyBattles-cardList'>
-          <div className='MyBattles-card'>
-            <h2 className='MyBattles-card-title'>Thèmes: Reflets</h2>
-            <p>Temps écoulé</p>
-            <p>Progress bar</p>
-            <button className='MyBattles-card-button'>Vote</button>
-            <p className='MyBattles-card-group'>Team Pix battle</p>
-          </div>
-        </div>
-        <StickyFooter />
-      </div>
-    )
-  }
+const MyBattles = () => {
+  return (
+    <div className='MyBattles-background'>
+      <Navbar />
+      <Menu />
+      <MyBattlesCardList />
+      <StickyFooter />
+    </div>
+  )
 }
 
 export default MyBattles
