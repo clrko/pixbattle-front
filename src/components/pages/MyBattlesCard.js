f536c61d51d4acef22eb027d58a278743860a60c
Claire Kodia <claire.kodia@gmail.com>
2020-07-10 12:29:27 +0200

    Funcitonnal verison of the Battle card

--- src/components/pages/MyBattlesCard.js
+++ src/components/pages/MyBattlesCard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './MyBattles.css'
 
-const MyBattlesCard = ({ battleTheme, battleTimeMessage, battleStatus, battleGroup }) => {
+const MyBattlesCard = ({ battleTheme, battleTimeMessage, battleStatus, battleGroup, userAdmin }) => {
   return (
     <div className='MyBattles-card'>
       <h2 className='MyBattles-card-title'>{battleTheme}</h2>
-      <i className='fas fa-crown MyBattles-crown' />
+      <i className={userAdmin ? 'fas fa-crown MyBattles-crown' : 'MyBattles-notAdmin'} />
       <p className='MyBattles-card-time'>{battleTimeMessage}</p>
       <button className='MyBattles-card-button'>{battleStatus}</button>
       <p className='MyBattles-card-group'>{battleGroup}</p>
