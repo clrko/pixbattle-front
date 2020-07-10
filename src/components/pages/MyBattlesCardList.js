790a95017ba784b38a3ded29cd6d0bb2b8a90e01
Claire Kodia <claire.kodia@gmail.com>
2020-07-10 12:30:36 +0200

    Mybattles card list component working with a hard coded battle infos

--- src/components/pages/MyBattlesCardList.js
+++ src/components/pages/MyBattlesCardList.js
@@ -7,25 +7,29 @@ const battleInfos = [
     battleTheme: 'Thème : Reflets',
     battleTimeMessage: 'Temps écoulé !',
     battleStatus: 'Vote',
-    battleGroup: 'Team Pix battle'
+    battleGroup: 'Team Pix battle',
+    userAdmin: true
   },
   {
     battleTheme: 'Thème : Flower Power',
     battleTimeMessage: 'Encore 6h 25min 35sec',
     battleStatus: 'Poste ta photo',
-    battleGroup: 'Wilders'
+    battleGroup: 'Wilders',
+    userAdmin: false
   },
   {
     battleTheme: 'Thème : Empilement',
     battleTimeMessage: 'Temps écoulé !',
     battleStatus: 'Resultats',
-    battleGroup: 'Team Pix battle'
+    battleGroup: 'Team Pix battle',
+    userAdmin: false
   },
   {
     battleTheme: 'Thème : Rayures',
     battleTimeMessage: 'Temps écoulé !',
-    battleStatus: 'Vote',
-    battleGroup: 'Team Hello'
+    battleStatus: 'Post', /* veiller à envoyer le battle ID */
+    battleGroup: 'Team Hello',
+    userAdmin: true
   }
 ]
 
@@ -33,7 +37,7 @@ class MyBattlesCardList extends Component {
   render () {
     return (
       <div className='MyBattles-cardList'>
-        {battleInfos.map((battle, i) => <MyBattlesCard battleTheme={battle.battleTheme} battleTimeMessage={battle.battleTimeMessage} battleStatus={battle.battleStatus} battleGroup={battle.battleGroup} key={i} />)}
+        {battleInfos.map((battle, i) => <MyBattlesCard battleTheme={battle.battleTheme} battleTimeMessage={battle.battleTimeMessage} battleStatus={battle.battleStatus} battleGroup={battle.battleGroup} key={i} userAdmin={battle.userAdmin} />)}
       </div>
     )
   }
