df6cc318cd7686f86d6a0b696bcb1897e3575133
Claire Kodia <claire.kodia@gmail.com>
2020-07-12 11:40:47 +0200

    Updated the urls with the username parameter

--- src/components/shared/DropDown.js
+++ src/components/shared/DropDown.js
@@ -1,25 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import './DropDown.css'
 
-class DropDown extends React.Component {
-  handleChange = e => {
-    this.props.history.push(e.target.value)
-  }
-
-  render () {
-    const { match } = this.props
-    const selectedOption = match.path
-    return (
-      <div className='background-DropDown'>
-        <select value={selectedOption} className='DropDown' onChange={this.handleChange}>
-          <option value='/:username'>Mon Profil</option>
-          <option value='/:username/ranking'>Mon Classement</option>
-          <option value='/:username/pictures'>Mes Photos</option>
-          <option value='/battles/:id/results'>Mon Podium</option>
-        </select>
-      </div>
-    )
-  }
+const mapStateToProps = state => {
+  const { user } = state
+  return { user }
 }
 
-export default DropDown
+const DropDown = ({ user, history, match }) => {
+  const handleChange = e => {
+    history.push(e.target.value)
+  }
+
+  const selectedOption = match.path
+
+  return (
+    <div className='background-DropDown'>
+      <select value={selectedOption} className='DropDown' onChange={handleChange}>
+        <option value={`/${user.username}`}>Mon Profil</option>
+        <option value={`/${user.username}/ranking`}>Mon Classement</option>
+        <option value={`/${user.username}/pictures`}>Mes Photos</option>
+        {/* <option value='/battles/:id/results'>Mon Podium</option> */} {/* composant à expliciter  */}
+      </select>
+    </div>
+  )
+}
+
+export default connect(mapStateToProps)(DropDown)
--- src/components/shared/StickyFooter.js
+++ src/components/shared/StickyFooter.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import './StickyFooter.css'
 
-const StickyFooter = () => {
+const mapStateToProps = state => {
+  const { user } = state
+  return { user }
+}
+
+const StickyFooter = ({ user }) => {
   return (
     <div className='div-StickyFooter'>
-      <NavLink to='/:username/battles' className='div-camera-StickyFooter'>
+      <NavLink to={`/${user.username}/battles`} className='div-camera-StickyFooter'>
         <i className='fa fa-camera' />
         <p>Battles</p>
       </NavLink>
-      <NavLink to='/:username/groups' className='div-users-StickyFooter'>
+      <NavLink to={`/${user.username}/groups`} className='div-users-StickyFooter'>
         <i className='fa fa-users' />
         <p>Groupes</p>
       </NavLink>
-      <NavLink to='/:username' className='div-user-StickyFooter'>
+      <NavLink to={`/${user.username}`} className='div-user-StickyFooter'>
         <i className='fa fa-user-circle' />
         <p>Profil</p>
       </NavLink>
@@ -21,4 +27,4 @@ const StickyFooter = () => {
   )
 }
 
-export default StickyFooter
+export default connect(mapStateToProps)(StickyFooter)
