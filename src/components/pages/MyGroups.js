b06a6396cb4e3209598ac40e11ec2c5353af0f6f
Claire Kodia <claire.kodia@gmail.com>
2020-07-17 19:27:32 +0200

    Working and cleaned version of the my group page

--- src/components/pages/MyGroups.js
+++ src/components/pages/MyGroups.js
@@ -1,45 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-// import { connect } from 'react-redux'
-// import Navbar from '../shared/Navbar'
+import { connect } from 'react-redux'
+import Navbar from '../shared/Navbar'
 import PageHeader from '../shared/PageHeader.js'
-// import StickyFooter from '../shared/StickyFooter'
+import StickyFooter from '../shared/StickyFooter'
 import './MyGroups.css'
 import MyGroupsCardList from './MyGroupsCardList.js'
 
-/* const mapStateToProps = state => {
+const mapStateToProps = state => {
   const { user } = state
   return { user }
-} */
+}
 
-const MyGroups = (/* { user } */) => {
-/*   const userGroupInformation = [
-    {
-      group_id: 1,
-      group_name: 'Team Pix Battle',
-      admin_user_id: 1,
-      ongoingBattles: 1,
-      finishedBattles: 2,
-      groupMembers: 6
-    },
-    {
-      group_id: 2,
-      group_name: 'Testgroup2',
-      admin_user_id: 5,
-      ongoingBattles: 2,
-      finishedBattles: 1,
-      groupMembers: 10
-    },
-    {
-      group_id: 3,
-      group_name: 'Testgroup5',
-      admin_user_id: 2,
-      ongoingBattles: 0,
-      finishedBattles: 0,
-      groupMembers: 4
-    }
-  ]
- */
+const MyGroups = ({ user }) => {
   const [userGroupInformation, setUserGroupInformation] = useState([])
 
   useEffect(() => {
@@ -86,19 +59,18 @@ const MyGroups = (/* { user } */) => {
 
   return (
     <div className='background-MyGroups'>
-      {/* <Navbar /> */}
+      <Navbar />
       <PageHeader pageTitle='Mes Groupes' />
       <MyGroupsCardList
         userGroupInformation={userGroupInformation}
-        userId={1}
+        userId={user.userId}
         getOngoingBattlesMessage={getOngoingBattlesMessage}
         getFinishedBattlesMessage={getFinishedBattlesMessage}
         getGroupMemberMessage={getGroupMemberMessage}
       />
-      {/* <StickyFooter /> */}
+      <StickyFooter />
     </div>
   )
 }
-/* Ne pas oublier d'ajouter le handleclick + recuperer le groupId, le userId */
-// export default connect(mapStateToProps)(MyGroups)
-export default MyGroups
+/* Ne pas oublier d'ajouter le handleclick */
+export default connect(mapStateToProps)(MyGroups)
