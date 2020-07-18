8a4cd98a3f000e880527e6e1a1559322aa466e14
Lola <lola.donval@gmail.com>
2020-07-18 13:31:47 +0200

    change name of lightbox for battle vote, create nex component to get ell photos af current user

--- src/components/pages/MyPictures.js
+++ src/components/pages/MyPictures.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 import Navbar from '../shared/Navbar'
 import DropDown from '../shared/DropDown'
+import Lightbox from '../shared/Lightbox'
 import StickyFooter from '../shared/StickyFooter'
 import './MyPictures.css'
 
 const Menu = withRouter(DropDown)
+const mapStateToProps = state => {
+  const { user } = state
+  return { user }
+}
 
-class MyPictures extends React.Component {
+class MyPictures extends Component {
+  state = {
+    photos: []
+  }
+
+  componentDidMount () {
+    this.getPhotos()
+  }
+
+  getPhotos = () => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/gallery/${this.props.user.userId}`,
+        {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        })
+      .then(res => this.setState({ photos: res.data }))
+  }
+
   render () {
+    const { photos } = this.state
     return (
       <div className='background-MyPictures'>
         <Navbar />
         <Menu />
         <div className='window-MyPictures'>
-          <p>Mes Photos</p>
+          <Lightbox photos={photos} />
         </div>
         <StickyFooter />
       </div>
@@ -22,4 +49,4 @@ class MyPictures extends React.Component {
   }
 }
 
-export default MyPictures
+export default connect(mapStateToProps)(MyPictures)
