f0d61ae9ed9d4ff37e4968e78251da5d609811a7
Lola <lola.donval@gmail.com>
2020-07-18 19:57:16 +0200

    add axios request to get battle reusults

--- src/components/pages/Podium.js
+++ src/components/pages/Podium.js
@@ -4,72 +4,28 @@ import { withRouter } from 'react-router-dom'
 import DropDown from '../shared/DropDown'
 import Navbar from '../shared/Navbar'
 import StickyFooter from '../shared/StickyFooter'
-import avatar from '../../asset/pictures/avatar_MyProfile.png'
-import avatar2 from '../../asset/pictures/avatar2.png'
-import avatar3 from '../../asset/pictures/avatar3.png'
-import avatar4 from '../../asset/pictures/avatar4.png'
 import './Podium.css'
 
 const Menu = withRouter(DropDown)
 
 class Podium extends React.Component {
   state = {
-    user: [
-      {
-        id: 1,
-        username: 'Marie',
-        avatar: avatar3,
-        victories: 9
-      },
-      {
-        id: 2,
-        username: 'Jean',
-        avatar: avatar2,
-        victories: 7
-      },
-      {
-        id: 3,
-        username: 'Paul',
-        avatar: avatar,
-        victories: 6
-      },
-      {
-        id: 4,
-        username: 'Aristide',
-        avatar: avatar4,
-        victories: 5
-      },
-      {
-        id: 5,
-        username: 'Marion',
-        avatar: avatar2,
-        victories: 3
-      },
-      {
-        id: 6,
-        username: 'Zoé',
-        avatar: avatar3,
-        victories: 2
-      },
-      {
-        id: 7,
-        username: 'Lola',
-        avatar: avatar3,
-        victories: 1
-      },
-      {
-        id: 8,
-        username: 'Julien',
-        avatar: avatar4,
-        victories: 0
-      },
-      {
-        id: 9,
-        username: 'Auxence',
-        avatar: avatar,
-        victories: 0
-      }
-    ]
+    users: []
+  }
+
+  componentDidMount () {
+    this.getResults()
+  }
+
+  getResults = () => {
+    const { battleId } = this.props.match.params
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/battle/${battleId}/results`)
+      .then(res =>
+        this.setState({
+          user: res.data
+        })
+      )
   }
 
   handleCreateGroupe = e => {
@@ -92,6 +48,12 @@ class Podium extends React.Component {
   }
 
   render () {
+    const { user } = this.state
+
+    if (user === undefined) {
+      return <p>loading...</p>
+    }
+
     return (
       <div>
         <Navbar />
@@ -100,53 +62,53 @@ class Podium extends React.Component {
           <div className='div-center-AvatarPodium'>
             <div className='AvatarPodium second-position'>
               <div className='div-p-fas'>
-                <p className='p-AvatarPodium'>{this.state.user[1].username}</p>
-                <i className='fas fa-star'><p className='p-user-victories-podium'>{this.state.user[1].victories}</p></i>
+                <p className='p-AvatarPodium'>{user[3].username}</p>
+                <i className='fas fa-star'><p className='p-user-victories-podium'>{user.victories}</p></i>
               </div>
               <div className='div-img-Avatar2'>
-                <img className='img-avatar-position' src={this.state.user[1].avatar} alt='avatar' />
+                <img className='img-avatar-position' src={user[3].avatar_url} alt='avatar' />
               </div>
               <i className='fas fa-medal medal2' />
             </div>
             <div className='AvatarPodium first-position'>
               <div className='div-p-fas'>
-                <p className='p-AvatarPodium'>{this.state.user[0].username}</p>
-                <i className='fas fa-star'><p className='p-user-victories-podium'>{this.state.user[0].victories}</p></i>
+                <p className='p-AvatarPodium'>{user[0].username}</p>
+                <i className='fas fa-star'><p className='p-user-victories-podium'>{user.victories}</p></i>
               </div>
               <div className='div-img-Avatar1'>
-                <img className='img-avatar-position' src={this.state.user[0].avatar} alt='avatar' />
+                <img className='img-avatar-position' src={user[0].avatar_url} alt='avatar' />
               </div>
               <i className='fas fa-medal medal1'><p /></i>
             </div>
             <div className='AvatarPodium third-position'>
               <div className='div-p-fas'>
-                <p className='p-AvatarPodium'>{this.state.user[2].username}</p>
-                <i className='fas fa-star'><p className='p-user-victories-podium'>{this.state.user[2].victories}</p></i>
+                <p className='p-AvatarPodium'>{user[6].username}</p>
+                <i className='fas fa-star'><p className='p-user-victories-podium'>{user.victories}</p></i>
               </div>
               <div className='div-img-Avatar3'>
-                <img className='img-avatar-position' src={this.state.user[2].avatar} alt='avatar' />
+                <img className='img-avatar-position' src={user[6].avatar_url} alt='avatar' />
               </div>
               <i className='fas fa-medal medal3' />
             </div>
           </div>
           <div className='div-congratulations'>
-            <h1 className='h1-div-congratulations'>Félicitations {this.state.user[0].username} !</h1>
+            <h1 className='h1-div-congratulations'>Félicitations {user[0].username} !</h1>
             <button className='button-createdNewGroup-MyProfile button-div-congratulations' onClick={this.handleCreateGroupe}>crée la prochaine battle</button>
           </div>
           <div className='div-attendee-list'>
-            {this.state.user.slice(3).map((user, i) => (
+            {user.slice(9).map((u, i) => (
               <div key={i} className='div-participant'>
                 <div className='margin-div-participant'>
-                  <p className='p-div-participant'>{user.id}.</p>
+                  <p className='p-div-participant'>{u.user_id}.</p>
                 </div>
                 <div className='margin-div-participant'>
-                  <img className='img-attendee-list' src={user.avatar} alt='avatar' />
+                  <img className='img-attendee-list' src={u.avatar_url} alt='avatar' />
                 </div>
                 <div className='margin-div-participant'>
-                  <p className='p-div-participant'>{user.username}</p>
+                  <p className='p-div-participant'>{u.username}</p>
                 </div>
                 <div className='margin-fa-star-attendee-list'>
-                  <i className='fas fa-star fa-star-attendee-list'><p className='p-user-victories'>{user.victories}</p></i>
+                  <i className='fas fa-star fa-star-attendee-list'><p className='p-user-victories'>{u.victories}</p></i>
                 </div>
               </div>
             ))}
