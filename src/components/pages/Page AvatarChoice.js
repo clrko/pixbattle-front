467c2e1fe0ec266a4a5da0f6178240f8b5c8d75a
Vincent <igabillevincent@outlook.fr>
2020-07-22 19:03:27 +0200

    AvatarChoice and FPS are created

---
+++ src/components/pages/Page AvatarChoice.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './PageAvatarChoice.css'
+import AvatarChoice from './AvatarChoice'
+
+const PageAvatarChoice = () => {
+  return (
+    <div className='background-PageAvatarChoice'>
+      <AvatarChoice />
+    </div>
+  )
+}
+
+export default PageAvatarChoice
