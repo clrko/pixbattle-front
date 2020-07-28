dd8d5615239454b15ffee0ea2c5fb7f908d2d37a
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-07-28 12:30:49 +0200

    Fix card lists (battles&groups) :v:

--- src/components/pages/MyBattlesCardList.js
+++ src/components/pages/MyBattlesCardList.js
@@ -4,18 +4,20 @@ import './MyBattles.css'
 
 const MyBattlesCardList = ({ userId, userBattleInformation, getCompletedPercentage, getBattleTimeMessage, getBattleStatus, handleClick, getIsAbleStatus, hasPosted }) => (
   <div className='MyBattles-cardList'>
-    {userBattleInformation.map((battle, i) =>
-      <MyBattlesCard
-        key={i}
-        battleTheme={battle.theme_name}
-        battleStatus={getBattleStatus(battle.status_name)}
-        battleGroup={battle.group_name}
-        userAdmin={battle.admin_user_id === userId}
-        completed={getCompletedPercentage(battle.create_date, battle.deadline, battle.status_name)}
-        battleTimeMessage={getBattleTimeMessage(battle.deadline, battle.status_name, hasPosted[battle.battle_id])}
-        handleClick={() => handleClick(battle.group_id, battle.battle_id, battle.status_name)}
-        showButton={getIsAbleStatus(battle.status_name, hasPosted[battle.battle_id])}
-      />)}
+    <div>
+      {userBattleInformation.map((battle, i) =>
+        <MyBattlesCard
+          key={i}
+          battleTheme={battle.theme_name}
+          battleStatus={getBattleStatus(battle.status_name)}
+          battleGroup={battle.group_name}
+          userAdmin={battle.admin_user_id === userId}
+          completed={getCompletedPercentage(battle.create_date, battle.deadline, battle.status_name)}
+          battleTimeMessage={getBattleTimeMessage(battle.deadline, battle.status_name, hasPosted[battle.battle_id])}
+          handleClick={() => handleClick(battle.group_id, battle.battle_id, battle.status_name)}
+          showButton={getIsAbleStatus(battle.status_name, hasPosted[battle.battle_id])}
+        />)}
+    </div>
   </div>
 )
 
